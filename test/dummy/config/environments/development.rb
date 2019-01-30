13405999d695c7d5869eb900dce0eb415ec3c185
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2019-01-30 15:33:12 +0100

    Unused files have been deleted, code has been cleaned up

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -39,8 +39,4 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
