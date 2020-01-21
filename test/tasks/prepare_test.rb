1c7b396d36902fb48b879356c4068d9a7b0acb4a
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2020-01-21 23:39:37 +0100

    [fix] It works for Rails 6.0.2.1

--- test/tasks/prepare_test.rb
+++ test/tasks/prepare_test.rb
@@ -4,28 +4,55 @@ require 'test_helper'
 require 'rake'
 
 class PrepareTest < ActiveSupport::TestCase
+  APP_JS_MAP_FILENAME = 'application.js-0c3ca5eaf24d04fc657ba1de3511dec1c1bb214ee56ce9074c19a273160869a1.map'
+  APP_CSS_MAP_FILENAME = 'application.css-955bd5a4593a7de76455672f49e450d0b278304b00ad4008028ebceedd47311a.map'
+
   def setup
     Rake::Task['assets:prepare'].invoke
   end
 
   def teardown
-    FileUtils.rm_r File.join(Rails.root, 'public', 'assets')
+    FileUtils.rm_r assets_path
   end
 
   test 'existence of files' do
-    assert File.file?(path_to('application.js-0c3ca5eaf24d04fc657ba1de3511dec1c1bb214ee56ce9074c19a273160869a1.map'))
-    assert File.file?(path_to('application.css-955bd5a4593a7de76455672f49e450d0b278304b00ad4008028ebceedd47311a.map'))
+    assert File.file?(path_to APP_JS_MAP_FILENAME)
+    assert File.file?(path_to APP_CSS_MAP_FILENAME)
 
-    assert File.file?(path_to('application-5ef0d5f24e906a0eb73352d4e25133b092c219380c75b622e0f6eed60dfe5785.js'))
-    assert File.file?(path_to('application-5ef0d5f24e906a0eb73352d4e25133b092c219380c75b622e0f6eed60dfe5785.js.gz'))
+    assert_equal APP_JS_MAP_FILENAME, extract_source_map_filename(last_line(app_path 'js'))
+    assert_equal APP_CSS_MAP_FILENAME, extract_source_map_filename(last_line(app_path 'css'))
 
-    assert File.file?(path_to('application-9e1b025beb7cab8061c1e58394f874261b9b5dc9aba7575b22ce33aa093d6f06.css'))
-    assert File.file?(path_to('application-9e1b025beb7cab8061c1e58394f874261b9b5dc9aba7575b22ce33aa093d6f06.css.gz'))
+    assert File.file?(app_path('js.gz'))
+    assert File.file?(app_path('css.gz'))
   end
 
   private
 
   def path_to file_name
-    File.join Rails.root, 'public', 'assets', file_name
+    File.join assets_path, file_name
+  end
+
+  def assets_path
+    File.join Rails.root, 'public', 'assets'
+  end
+
+  def last_line file_path
+    File.readlines(file_path)[-1]
+  end
+
+  def assets
+    `ls #{assets_path}`.split("\n")
+  end
+
+  def app_filename ext
+    assets.find { |name| name =~ /application\-[0-9a-f]+\.#{ext}/ }
+  end
+
+  def app_path ext
+    File.join assets_path, app_filename(ext)
+  end
+
+  def extract_source_map_filename line
+    line.split('sourceMappingURL=').last.chomp.sub(';', '').sub('*/', '')
   end
 end
